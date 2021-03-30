ddb795b3230f89756e588378dbc00d6ab488ca21
mderome <dr4icus59@gmail.com>
2021-03-30 12:57:46 +0200

    premiergit

---
+++ ft_isalpha.c
@@ -0,0 +1,7 @@
+int	ft_isalpha(int c)
+{
+	if (((char)c >= 'a' && char(c) <= 'z')
+		|| ((char)c >= 'A' && (char)c <= 'Z'))
+		return (1);
+	return (0);
+}
---
+++ libft.h
@@ -0,0 +1,10 @@
+#ifndef _LIBFT_H_
+#define _LIBFT_H_
+
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
